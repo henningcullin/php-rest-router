74beb15d96c302d78da0841767999b48748189e0
Henning Cullin <henningcullin@gmail.com>
2024-10-09 18:44:53 +0200

    refactor: delete old test

--- tests/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once('../Router.php');
-require_once('product.php');
-
-Router::post('/', 'Product::details');
-
-Router::listen('/php-rest-router/tests');
--- tests/product.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Product {
-
-    public static function details() {
-        var_dump(Router::$body);
-    }
-
-}
